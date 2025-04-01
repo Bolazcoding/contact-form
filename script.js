e27608f247868f9aa440ffc482547b01687dd4e4
Adelabu Mobolaji <142588160+Bolazcoding@users.noreply.github.com>
2025-04-01 17:30:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const lastName = document.querySelector("#lname");
 const email = document.querySelector("#email");
 const queries = document.querySelectorAll(".query-radio");
 
-const email_error = document.querySelector(".email-error");
+const email_error = document.querySelector("#email-error");
 const error_query = document.querySelector("#dispQueryError");
 const error_term = document.querySelector("#dispTermError");
 const error_message = document.querySelectorAll(".error-msg");
 const inputs = document.querySelectorAll(".input-field");
 
+const success_container = document.querySelector(".success-container");
+const contact_form = document.querySelector("#contact-form");
+
 const submitBtn = document.querySelector(".btn");
 
-let fName, lName, comment, queryValue, termValue;
+// Global Variables
+let fName, lName, comment, emailValue, queryValue, termValue;
 
 // Checks if a query type is selected
 function querySelected() {
@@ -39,7 +43,16 @@ function termSelected() {
 // Submit the form after the validation is complete
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  const emailValue = email.value.trim();
+
+  if (formValidation()) {
+    showSuccessMessage();
+    document.getElementById("contact-form").reset();
+  }
+});
+
+// The function to validate the input fields
+function formValidation() {
+  emailValue = email.value.trim();
 
   fName = firstName.value.trim();
   lName = lastName.value.trim();
@@ -51,8 +64,9 @@ submitBtn.addEventListener("click", function (e) {
       input.style.borderColor = "hsl(0, 66%, 54%)";
       input.style.color = "hsl(187, 24%, 22%)";
       error_message[i].style.display = "block";
+      return;
     } else {
-      input.style.borderColor = "hsl(169, 82%, 27%)";
+      input.style.borderColor = "hsl(186, 15%, 59%)";
       input.style.backgroundColor = "hsl(0, 0%, 100%)";
       error_message[i].style.display = "none";
     }
@@ -63,13 +77,15 @@ submitBtn.addEventListener("click", function (e) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (!emailRegex.test(emailValue)) {
-    email_error.textContent = " Please enter a valid email address";
+    email_error.textContent = "Please enter a valid email address";
     email.style.borderColor = "hsl(0, 66%, 54%)";
     email.style.color = "hsl(187, 24%, 22%)";
+    return;
   }
 
   if (!queryValue) {
     error_query.textContent = "Please select a query type";
+    return;
   } else {
     error_query.textContent = "";
   }
@@ -77,10 +93,22 @@ submitBtn.addEventListener("click", function (e) {
   if (!termValue) {
     error_term.textContent =
       " To submit this form, please consent to being contacted";
+    return;
   } else {
     error_term.textContent = "";
   }
-});
+
+  return true;
+}
+
+// the function to show the success message
+function showSuccessMessage() {
+  success_container.style.display = "block";
+
+  setTimeout(() => {
+    success_container.style.display = "none";
+  }, 5000);
+}
 
 querySelected();
 termSelected();
